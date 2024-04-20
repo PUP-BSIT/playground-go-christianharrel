81a2501bdbf511cecc6c216e37774e5ae9004790
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-04-20 14:18:42 +0800

    add *ngFor practice

--- angualr_intro/src/app/app.component.ts
+++ angualr_intro/src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   operand_1: number;
@@ -12,9 +12,10 @@ export class AppComponent {
   isActive = true;
   inactive: any;
   isLoggedIn = false;
-
+  students = ['student1', 'student2', 'student3'];
+  
   performOperation(operation: string): void {
-    switch(operation) {
+    switch (operation) {
       case 'ADD':
         this.result = this.operand_1 + this.operand_2;
         break;
