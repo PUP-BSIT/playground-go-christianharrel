e7e74c4e54ff016bd4e4ed177a9f878720d1f17b
go-christianharrel <chrome.go.christianharrel@gmail.com>
2024-05-11 14:39:18 +0800

    add jquery

--- my-reactive-form/src/app/app.component.ts
+++ my-reactive-form/src/app/app.component.ts
@@ -1,10 +1,13 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'my-reactive-form';
+  ngOnInit(): void {
+}
 }
